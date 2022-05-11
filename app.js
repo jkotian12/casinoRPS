5396a3c9e24ccfcc93245e0edfdad9b93425ac82
Jathin Kotian <jkmanipaladmission@gmail.com>
2022-05-11 20:40:08 +0530

    final commit

--- app.js
+++ app.js
@@ -5,30 +5,55 @@ const choices = document.querySelectorAll(".choice");
 const result = document.getElementById("result");
 const playerChoiceDisplay = document.getElementById("playerChoice");
 const enemyChoiceDisplay = document.getElementById("enemyChoice");
-const scoreDisplay = document.getElementById("score");
-const header = document.getElementById("header");
 const reset = document.getElementById("reset");
 const nextHand = document.getElementById("next");
 const body = document.getElementById("body");
+const player1 = document.getElementById("player1");
+const player2 = document.getElementById("player2");
+const playerScore = document.getElementById("pscore");
+const player1Score = document.getElementById("p1score");
+const player2Score = document.getElementById("p2score");
+const enemyScore = document.getElementById("escore");
 
 //initialize score
-let score = 0;
+let pscore = 0;
+let escore = 0;
 
 //IIFE to initialise the game
 const game = (function () {
-  header.innerHTML = "Make your Choice";
-  scoreDisplay.innerHTML = `Score:${score}`;
+  player1.innerHTML = `Casino`;
+  player2.innerHTML = `You`;
+  playerScore.innerHTML = `${pscore}`;
+  enemyScore.innerHTML = `${escore}`;
+  result.innerHTML = "Choose your hand from the bottom three cards";
+  enemyChoiceDisplay.style.backgroundImage = 'url("logo.jpg")';
+
+  //reset button functionality
   reset.addEventListener("click", () => {
-    score = 0;
-    scoreDisplay.innerHTML = `Score:${score}`;
-    result.innerHTML = "";
+    pscore = 0;
+    escore = 0;
+    playerScore.innerHTML = `${pscore}`;
+    enemyScore.innerHTML = `${escore}`;
+    result.innerHTML = "Choose your hand from the bottom three cards";
+    choices.forEach((choice) => {
+      for (let i = 0; i < choice.classList.length; i++) {
+        if (choice.classList[i] == "leftChosen") {
+          choice.classList.remove("leftChosen");
+        }
+        if (choice.classList[i] == "middleChosen") {
+          choice.classList.remove("middleChosen");
+        }
+        if (choice.classList[i] == "rightChosen") {
+          choice.classList.remove("rightChosen");
+        }
+      }
+    });
   });
 })();
 
-//adding event listeners for each playable option 
+//adding event listeners for each playable option\
 choices.forEach((choice) => {
   choice.addEventListener("click", () => {
-    body.classList.add("blurred")
     playerChoice = choice.getAttribute("playerChoice");
     let position = choice.getAttribute("position");
     if (position == "left") {
@@ -40,12 +65,12 @@ choices.forEach((choice) => {
     if (position == "right") {
       choice.classList.add("rightChosen");
     }
-    compareChoices(playerChoice, choice);
+    playerComparison(playerChoice, choice);
   });
 });
 
 //Function for comparing player and computer choices
-const compareChoices = function (playChoice, choice) {
+const playerComparison = function (playChoice, choice) {
   const choices = ["rock", "paper", "scissors"];
   let enemyChoice = choices[Math.floor(Math.random() * 3)];
   enemyChoiceDisplay.style.backgroundImage = `url("./${enemyChoice}.jpg")`;
@@ -57,35 +82,37 @@ const compareChoices = function (playChoice, choice) {
     (playChoice === "scissors" && enemyChoice === "paper")
   ) {
     result.innerHTML = "You Win";
-    updateScore(1);
+    console.log(updateScore(1, 0));
   } else {
     result.innerHTML = "You Lose";
-    updateScore(-1);
+    console.log(updateScore(0, 1));
   }
-    nextHand.style.display="block";
-    nextHand.addEventListener("click", ()=>{
-      newHand(playChoice, choice);
-    })
+  nextHand.style.display = "block";
+  nextHand.addEventListener("click", () => {
+    newHand(playChoice, choice);
+  });
 };
 
 //Function for updating the score after every play
-const updateScore = function (update) {
-  score = score + update;
-  scoreDisplay.innerHTML = `${score}`;
+const updateScore = function (player, enemy) {
+  pscore = pscore + player;
+  escore = escore + enemy;
+  playerScore.innerHTML = `${pscore}`;
+  enemyScore.innerHTML = `${escore}`;
 };
 
 //Function for the next play
 const newHand = function (playerChoice, choice) {
-  for(let i = 0 ;i<choice.classList.length;i++){
-      if(choice.classList[i]=="leftChosen"){
-        choice.classList.remove("leftChosen");
-
-      }
-      if(choice.classList[i]=="middleChosen"){
-        choice.classList.remove("middleChosen");
-      }
-      if(choice.classList[i]=="rightChosen"){
-        choice.classList.remove("rightChosen");
-      }
+  for (let i = 0; i < choice.classList.length; i++) {
+    if (choice.classList[i] == "leftChosen") {
+      choice.classList.remove("leftChosen");
+    }
+    if (choice.classList[i] == "middleChosen") {
+      choice.classList.remove("middleChosen");
+    }
+    if (choice.classList[i] == "rightChosen") {
+      choice.classList.remove("rightChosen");
+    }
   }
+  enemyChoiceDisplay.style.backgroundImage = 'url("logo.jpg")';
 };
